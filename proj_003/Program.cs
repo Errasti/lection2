f9eb17a4fed979e5b527fe90888fdc043eb0370d
Francisko <errasti007@yandex.ru>
2022-07-11 00:32:01 +0300

    modified

--- proj_003/Program.cs
+++ proj_003/Program.cs
@@ -21,8 +21,28 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOff( int[] collection,int find )
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 
-int [] array = new int[10];
+int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOff(array, 4);
+Console.WriteLine(pos);
