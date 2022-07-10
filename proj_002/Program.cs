212fa80370ed3e9a74466cc75bfb6a9b9d0190b1
Francisko <errasti007@yandex.ru>
2022-07-10 23:30:30 +0300

    proj 2 commition

---
+++ proj_002/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 16, 20, 31, 23, 18, 50, 4, 23, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find )
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
